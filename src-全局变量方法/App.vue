4a399036e93c98a072eb628937c0d095bd3c63dc
Cissy <1395422644@qq.com>
2023-12-12 11:27:55 +0800

    feat:全局变量，全局方法

---
+++ src-全局变量方法/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+ import A from './components/A.vue'
+ 
+
+ import { ref} from "vue";
+
+</script>
+
+<template>
+    <A></A>
+    
+    
+</template>
+
+<style scoped>
+
+</style>
