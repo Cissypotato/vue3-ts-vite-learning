fcad871664b199041667eab525c96055c3e3b6dd
Cissy <1395422644@qq.com>
2023-12-17 20:17:20 +0800

    feat:scoped与样式穿透

---
+++ src-scoped/components/B.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <p class="b-p">cat cata</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref, reactive } from "vue";
+// const color = ref("red");
+const style= ref({color:"red"});
+setTimeout(() => {
+//   color.value = "pink";
+style.value.color='green'
+}, 3000);
+</script>
+<style scoped>
+.b-p {
+  /* color: v-bind(color); */
+  color: v-bind('style.color');/**对象的话要加单引号 */
+}
+</style>
