da432687e4ac5027802429fe4b36add5fea90a92
Cissy <1395422644@qq.com>
2024-01-02 15:19:58 +0800

    init

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,5 @@
-import { createApp } from "vue";
-import { createPinia } from "pinia";
-import "./style.css";
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
 
-import App from "./App.vue";
-
-const store = createPinia();
-const app = createApp(App);
-
-app.use(store);
-
-app.mount("#app");
+createApp(App).mount('#app')
